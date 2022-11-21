7ee82c09ae2feef2efcf2a98e10ecefe53efb127
yoonhee31 <yhmelody31@gmail.com>
2022-11-21 21:58:47 +0900

    feat : 로그인로직 기초 완성

---
+++ server/router/user.js
@@ -0,0 +1,41 @@
+import express from "express";
+import "express-async-errors";
+
+let users = [
+  {
+    id: "1",
+    username: "yoon",
+    password: "1234",
+    name: "yoon",
+    email: "yoon@naver.com",
+  },
+  {
+    id: "2",
+    username: "hee",
+    password: "12345",
+    name: "hee",
+    email: "hee@naver.com",
+  },
+];
+
+const router = express.Router();
+
+router.post("/login", (req, res, next) => {
+  const { email, password } = req.body;
+  const isValidEmail = users.find((user) => user.email === email);
+  const isValidPassword = users.find((user) => user.password === password);
+  if (!isValidEmail) {
+    return res
+      .status(401)
+      .json({ message: "이메일 또는 비밀번호가 맞지 않습니다." });
+  }
+  if (!isValidPassword) {
+    return res
+      .status(401)
+      .json({ message: "이메일 또는 비밀번호가 맞지 않습니다." });
+  }
+  req.session.save(email);
+  res.status(200).json({ message: "로그인성공" });
+});
+
+export default router;
