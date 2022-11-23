e0e3767d7a9a9950c903d6f00740871902a64516
yoonhee31 <yhmelody31@gmail.com>
2022-11-23 21:07:36 +0900

    feat:로그인로직완성

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
       </Routes>
     </BrowserRouter>
   );
